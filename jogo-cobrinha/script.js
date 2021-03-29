872544292b956906ffa6a67b98e28f1a98fdc91a
HericlisBlip <hericlisventura.o@gmail.com>
2021-03-29 05:12:53 -0300

    Definindo Direções e impedir que a cobrinha suma.

--- jogo-cobrinha/script.js
+++ jogo-cobrinha/script.js
@@ -21,7 +21,22 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener("keydown", update);
+
+function update (event){
+    if(event.keyCode == 37 && direction !="right") direction = "left";
+    if(event.keyCode == 38 && direction != "down" ) direction = "right";
+    if(event.keyCode == 39 && direction != "left" ) direction = "right";
+    if(event.keyCode == 40  && direction != "up" ) direction = "down";
+}
+
+
 function iniciarJogo(){
+    if(snake[0].x > 15* box && direction =="right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction =="leght") snake[0].x = 16 * box;
+    if(snake[0].y > 15* box && direction =="down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction =="up") snake[0].y = 16* box ;
+    
     criarBG(); 
     criarCobrinha();   
 
@@ -39,7 +54,7 @@ function iniciarJogo(){
         y: snakeY
     }
 
-        snake.unshift(newHead);
+    snake.unshift(newHead);
 
 }
 let jogo = setInterval(iniciarJogo, 100)
