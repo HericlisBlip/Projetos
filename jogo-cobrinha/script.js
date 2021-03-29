4fdf715202b49c72be5ef0d7dab52bfaff8e48c0
HericlisBlip <hericlisventura.o@gmail.com>
2021-03-29 06:11:30 -0300

    Reparendo pequenos erro de digitação do codigo.

--- jogo-cobrinha/script.js
+++ jogo-cobrinha/script.js
@@ -6,7 +6,7 @@ snake [0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direction =" right";
+let direction ="right";
 
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
@@ -43,10 +43,10 @@ function update (event){
 
 function iniciarJogo(){
    
-    if(snake[0].x > 15* box && direction =="right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction =='leght') snake[0].x = 16 * box;
-    if(snake[0].y > 15* box && direction =="down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction =='up') snake[0].y = 16* box ;
+    if(snake[0].x > 15*box && direction =="right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction =='left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction =="down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction =='up') snake[0].y = 16 * box ;
     
     for(i= 1; i< snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
@@ -65,7 +65,7 @@ function iniciarJogo(){
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -=box;
-    if(direction =="up") snakeY -= box;
+    if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
     
     if(snakeX != food.x || snakeY != food.y){
@@ -73,8 +73,8 @@ function iniciarJogo(){
     
     }
     else{
-        food.x = Math.floor(Math.random() * 15 + 1) * box;
-        food.y = Math.floor(Math.random() * 15 + 1) * box; 
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box; 
     }
 
     let newHead ={
